0c330da4c1cb6f6ba9d421ffdfccf1079ac7157e
Maxim Dmitruk <39312751+SimaDmtr@users.noreply.github.com>
2018-09-06 00:56:32 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,14 +2,25 @@ $(document).ready(function () {
     $('.tab-content').hide();
     $('.tab-content:first').show();
     $('.tabs li:first').addClass('active');
-
-    $('.tabs li').click(function () {
-        $('.tabs  li').removeClass('active');
-        $(this).addClass('active');
+    $('.tabs a').click(function () {
         $('.tab-content').hide();
-        var selectTab = $(this).find('a').attr("href");
+        var selectTab = $(this).attr("href");
         $(selectTab).fadeIn();
         return false;
+    });
+
+    $('.change').click(function(){
+        let visibleTab  = $('.tabs-container :visible').first();
+        let canChangeUp = $(visibleTab).next('.tab-content').length;
+        let canChangeDown = $(visibleTab).prev('.tab-content').length;
+        if ($(this).hasClass('next') && canChangeUp){
+            visibleTab.hide();
+            $(visibleTab).next('.tab-content').fadeIn().show()
+        }
+        if ($(this).hasClass('prev') && canChangeDown){
+            visibleTab.hide();
+            $(visibleTab).prev('.tab-content').fadeIn().show()
+        }
     });
     $(document).ready(function () {
         $(".owl-carousel").owlCarousel({
