b15af71bf26d169850ac61a1475a6a9f68fb94e6
Maxim Dmitruk <39312751+SimaDmtr@users.noreply.github.com>
2018-09-06 19:36:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,34 @@
 $(document).ready(function () {
     $('.tab-content').hide();
     $('.tab-content:first').show();
-    $('.tabs li:first').addClass('active');
+    $('.tabItem:first').addClass('active');
     $('.tabs a').click(function () {
+        let tabs = $('.active');
+        $(tabs).removeClass('active');
+        $(this).parent().addClass('active');
         $('.tab-content').hide();
         var selectTab = $(this).attr("href");
         $(selectTab).fadeIn();
         return false;
     });
-
     $('.change').click(function(){
+        let tabs = $('.active');
         let visibleTab  = $('.tabs-container :visible').first();
         let canChangeUp = $(visibleTab).next('.tab-content').length;
         let canChangeDown = $(visibleTab).prev('.tab-content').length;
         if ($(this).hasClass('next') && canChangeUp){
+            tabs.removeClass('active');
+            tabs.next('.tabItem').addClass('active');
             visibleTab.hide();
-            $(visibleTab).next('.tab-content').fadeIn().show()
+            $(visibleTab).next('.tab-content').fadeIn()
         }
         if ($(this).hasClass('prev') && canChangeDown){
+            tabs.removeClass('active');
+            tabs.prev('.tabItem').addClass('active');
             visibleTab.hide();
-            $(visibleTab).prev('.tab-content').fadeIn().show()
+            $(visibleTab).prev('.tab-content').fadeIn()
         }
-    });
+    })
     $(document).ready(function () {
         $(".owl-carousel").owlCarousel({
             items: 1,
